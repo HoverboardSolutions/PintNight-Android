d1a9334b44eae1603dd1f850f9d2169c70527ce1
aleedy <aleedy@gmail.com>
2012-03-06 13:36:36 -0600

    Initial Commit of Pint Night for Android

---
+++ PintNight/src/pint/night/PintNightActivity.java
@@ -0,0 +1,211 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pint.night;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
+import android.app.Activity;
+
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import org.jsoup.select.Elements;
+import org.jsoup.Jsoup;
+
+import android.app.AlertDialog;
+import android.content.res.Resources;
+import android.webkit.WebView;
+import android.os.Bundle;
+import android.content.*;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.view.View;
+//import android.view.View.OnClickListener;
+//import android.widget.Button;
+//import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
+
+import com.google.ads.*;
+
+/**
+ * This class provides a basic demonstration of how to write an Android
+ * activity. Inside of its window, it places a single view: an EditText that
+ * displays and edits some internal text.
+ */
+public class PintNightActivity extends Activity implements OnTabChangeListener{
+    WebView upcoming_browser;
+    WebView calendar_browser;
+    WebView collection_browser;
+    ListView calendar_list;
+    
+   // PintNightEvent pintNightEvents[];
+
+    
+    private List<PintNightEvent> pintNightEvents = new ArrayList<PintNightEvent>(10);
+    private ArrayList<String> beerList=new ArrayList<String>();	
+    private String[] beerListArray;
+    public PintNightActivity() {
+    }
+
+    /** Called with the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Inflate our UI from its XML layout description.
+        setContentView(R.layout.main);
+        
+        AdView adView = (AdView)this.findViewById(R.id.adView);
+        adView.loadAd(new AdRequest());	
+        
+       
+        Resources res = getResources();
+        
+        TabHost tabs=(TabHost)findViewById(R.id.tabhost);
+        tabs.setup();
+        
+        TabHost.TabSpec spec=tabs.newTabSpec("tag1");
+        spec.setContent(R.id.upcoming);
+        spec.setIndicator("",res.getDrawable(R.drawable.ic_tab_upcoming));
+        tabs.addTab(spec);
+        
+        spec=tabs.newTabSpec("tag2");
+        spec.setContent(R.id.ListView01);
+        spec.setIndicator("", res.getDrawable(R.drawable.ic_tab_calendar));
+        tabs.addTab(spec);
+        
+        spec=tabs.newTabSpec("collection");
+        spec.setContent(R.id.collection);
+        spec.setIndicator("",res.getDrawable(R.drawable.ic_tab_collection));
+        tabs.addTab(spec);
+        
+        tabs.setOnTabChangedListener(this);
+        try{
+        	parseCalender();
+        }catch (IOException a){
+        	
+        }
+      //  fillListViewString();
+        calendar_list = (ListView)findViewById(R.id.ListView01);
+        calendar_list.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, beerListArray)); 
+                
+        
+        upcoming_browser=(WebView)findViewById(R.id.upcoming);
+        upcoming_browser.loadUrl("http://schmidtcds.com/pintnight/v2/upcoming2.php");
+     //   calendar_browser=(WebView)findViewById(R.id.calendar);
+     //   calendar_browser.loadUrl("http://schmidtcds.com/pintnight/v2/pintcalendar2.php");
+        collection_browser=(WebView)findViewById(R.id.collection);
+        collection_browser.loadUrl("http://ad.leadboltads.net/show_app_wall?section_id=504415991");
+      
+        
+
+        
+    }
+    
+    @Override
+    public void onTabChanged(String tabId) {
+    	if(tabId.equals("collection")){
+            showInDevelopmentAlert();
+            }
+    	
+    }
+    
+    
+    public void showInDevelopmentAlert() {
+    	AlertDialog alertDialog = new AlertDialog.Builder(this).create(); 
+    	alertDialog.setMessage("The Collections feature is currently in development.  If you would like to support this and other features, please check out our sponsors");
+    	
+    	alertDialog.setTitle("Feature in Development");
+    	alertDialog.setButton("Cheers!", new DialogInterface.OnClickListener() {  
+    	      public void onClick(DialogInterface dialog, int which) {  
+    	        return;  
+    	    } });
+    	alertDialog.show();
+    	
+    }
+    
+    
+    public void testAlert(String message) {
+    	AlertDialog alertDialog = new AlertDialog.Builder(this).create(); 
+    	alertDialog.setMessage(message);
+    	
+    	alertDialog.setTitle("Feature in Development");
+    	alertDialog.setButton("Cheers!", new DialogInterface.OnClickListener() {  
+    	      public void onClick(DialogInterface dialog, int which) {  
+    	        return;  
+    	    } });
+    	alertDialog.show();
+    	
+    }
+    
+    public void parseCalender() throws IOException {
+    	
+    	try {
+    		Document calender = Jsoup.connect("http://www.pazzospizzapub.com/PintNight.html").get();
+    		Elements tables = calender.getElementsByTag("table");
+    		Elements events = tables.get(2).getElementsByTag("tr");
+    		
+    		beerList.ensureCapacity(events.size());
+    		
+    		PintNightEvent event = new PintNightEvent();
+    		for(int k = 0; k < events.size(); k++){
+    			event = new PintNightEvent(events.get(k));
+    			//testAlert(Integer.toString(events.size()));
+    			beerList.add(event.toString());
+    		}	
+    		beerListArray = new String[beerList.size()];
+    		beerList.toArray(beerListArray);
+    		
+    	} catch (IOException a) { 	
+    	} catch (IndexOutOfBoundsException a) {
+    		testAlert(a.toString());
+		} catch (NullPointerException a) {
+    		testAlert(a.toString());
+		}	
+    	
+    }
+  /*  public void fillListViewString(){
+    		
+    		Iterator<PintNightEvent> eventsIterator = pintNightEvents.listIterator();
+    		try {
+    			while(eventsIterator.hasNext()){
+    				beerList.add(eventsIterator.next().getBeer());
+    			}
+    		} catch (IndexOutOfBoundsException a){
+    			
+    		}
+    		beerListArray = new String[beerList.size()];
+    		beerList.toArray(beerListArray);
+    	}*/
+    
+}
+
+    /**
+     * Called when the activity is about to start interacting with the user.
+     */
+  //  @Override
+   // protected void onResume() {
+    //    super.onResume();
+   // }
+
+  
+
---
+++ PintNight/src/pint/night/PintNightEvent.java
@@ -0,0 +1,66 @@
+package pint.night;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class PintNightEvent {
+	
+private String date;
+private String beer;
+private String abv;
+private String description;
+private String notes;
+private Elements pint;
+
+public PintNightEvent(Element pintNight){
+	pint = pintNight.getElementsByTag("font");
+    date = pint.get(0).text();
+	beer = pint.get(2).text();
+	abv = pint.get(4).text();
+	description = pint.get(6).text();
+	notes = pint.get(8).text();
+}
+
+public PintNightEvent(){
+    date = "n/a";
+	beer = "n/a";
+	abv = "n/a";
+	description = "n/a";
+	notes = "n/a";
+}
+
+public String toString(){
+	//Occasionally the table on the PintNight website isn't fully populated
+	//This results in a series of dashes, up to six
+	//removing three, and then two and the replace the last one with newline
+	//should take care of any possible number of dashes up to 6
+	
+	String invalidFieldsRemoved = pint.text().replace("- - - ", "");
+	invalidFieldsRemoved = pint.text().replace("- - ", "");
+	return invalidFieldsRemoved.replace("-", "\n  ");
+	}
+
+public String moreInfo(){
+	return "<a href=\"" + "http://www.google.com/search?btnI=1&as_sitesearch=beeradvocate.com&q=" + beer + "\">" + "More Info" + "<a>";
+}
+
+public String getDate(){
+	return date;
+}
+
+public String getBeer(){
+	return beer;
+}
+
+public String getABV(){
+	return abv;
+}
+
+public String getDescription(){
+	return description;
+}
+
+public String getNotes(){
+	return notes;
+}
+}
